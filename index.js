3803a371d9566986efc17493921b016bb9321818
Alexandre Paquette <alexandre.paquette@nadrox.com>
2016-10-07 21:19:37 -0400

    using express implementation for engine.io on heroku

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 'use strict';
-let engine = require('engine.io');
+const engine = require('engine.io');
+const express = require('express');
+const messageFactory = require('./JsonMessageFactory');
 
-let messageFactory = require('./JsonMessageFactory');
+const heroesMsg = messageFactory.createMessage('heroes', require('./data/heroes.json')).toString();
+const matchingMsg = messageFactory.createMessage('matching', require('./data/matching.json')).toString();
+const mapsMsg = messageFactory.createMessage('maps',  require('./data/maps.json')).toString();
 
-let heroesMsg = messageFactory.createMessage('heroes', require('./data/heroes.json')).toString();
-let matchingMsg = messageFactory.createMessage('matching', require('./data/matching.json')).toString();
-let mapsMsg = messageFactory.createMessage('maps',  require('./data/maps.json')).toString();
+const PORT = process.env.PORT || 7654;
+const server = express()
+    .use((req, res) => res.send('ok') )
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-let server = engine.listen(80);
+let ws = engine(server);
 
-server.on('connection', function (socket) {
+ws.on('connection', function (socket) {
     console.log('ccnnection requested');
     socket.send(heroesMsg);
     socket.send(matchingMsg);
